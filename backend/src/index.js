3fb6d31a0e92de0c593b8d9874ac0d66f25800ef
Gaurav Sharma <gau1711sha@gmail.com>
2025-07-31 23:36:33 +0530

    basic setup done

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+
+import connectDB from './db/db.js'
+
+import  dotenv from 'dotenv'
+dotenv.config()
+
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:5173'
+}))
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`server listning at ${PORT}`);
+    
+})
