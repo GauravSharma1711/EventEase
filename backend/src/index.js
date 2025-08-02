9370125de3d8d2293f34d197f9f09a7233177b9e
Gaurav Sharma <gau1711sha@gmail.com>
2025-08-03 00:43:33 +0530

    -

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,17 +15,17 @@ import { fileURLToPath } from 'url';
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const frontendPath = path.resolve(__dirname, "../../../frontend/dist");
+const frontendPath = path.resolve(__dirname, "../../frontend/dist");
 
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS only in development
+
 if (process.env.NODE_ENV.trim() !== 'production') {
   app.use(
     cors({
@@ -35,21 +35,21 @@ if (process.env.NODE_ENV.trim() !== 'production') {
   );
 }
 
-// Routes
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/event', eventRoutes);
 app.use('/api/v1/booking', bookingRoutes);
 
-// Serve static files from frontend in production
+
 if (process.env.NODE_ENV.trim() === 'production') {
-app.use(express.static(frontendPath));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
+  app.use(express.static(frontendPath));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'index.html'));
+  });
 }
 
-// Start server
+
 app.listen(PORT, () => {
   connectDB();
-  console.log(`server listening at ${PORT}`);
+  console.log(`Server listening at port ${PORT}`);
 });
